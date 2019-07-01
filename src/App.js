26e92ba25da45ac4e0c908a3d5ae902950c0db47
nbeers22 <nbeers22@gmail.com>
2019-07-01 12:34:06 -0400

    RTeact bookmarks app update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+
 import AddBookmark from './AddBookmark/AddBookmark';
 import BookmarkList from './BookmarkList/BookmarkList';
 import Nav from './Nav/Nav';
 import config from './config';
+
 import './App.css';
 
 const bookmarks = [
@@ -31,20 +34,14 @@ const bookmarks = [
 
 class App extends Component {
   state = {
-    page: 'list',
     bookmarks,
     error: null,
   };
-
-  changePage = (page) => {
-    this.setState({ page })
-  }
 
   setBookmarks = bookmarks => {
     this.setState({
       bookmarks,
       error: null,
-      page: 'list',
     })
   }
 
@@ -73,23 +70,31 @@ class App extends Component {
   }
 
   render() {
-    const { page, bookmarks } = this.state
     return (
       <main className='App'>
         <h1>Bookmarks!</h1>
-        <Nav clickPage={this.changePage} />
+        <Nav />
         <div className='content' aria-live='polite'>
-          {page === 'add' && (
-            <AddBookmark
-              onAddBookmark={this.addBookmark}
-              onClickCancel={() => this.changePage('list')}
-            />
-          )}
-          {page === 'list' && (
-            <BookmarkList
-              bookmarks={bookmarks}
-            />
-          )}
+          <Route
+            path='/add-bookmark'
+            render={({history}) => {
+              console.log(history)
+              return (
+                <AddBookmark
+                  onAddBookmark={this.addBookmark}
+                  onClickCancel={() => history.push('/')}
+                />
+              )
+            }}
+          />
+          <Route
+            exact
+            path='/'
+            render={() => 
+              <BookmarkList
+                bookmarks={bookmarks}
+              />}
+          />
         </div>
       </main>
     );
--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
-  API_KEY: '$2a$10$ra1z0n2XnSnbMP/ipTMHeOqqrI7i8Rssm/z8MHTxgb7LamV7LpfXu',
+  API_KEY: '$2a$10$k4jEZPwmEIOA2258RxKO4O4Tw55LW87O3SPXGu4Hi/Zk03E8yc7rm',
 }
